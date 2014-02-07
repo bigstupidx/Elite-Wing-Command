04ce01846b66605f4d2ff70da13a83e0b0576fbc
Kevin Salchert <kevin.salchert@me.com>
2014-02-06 22:56:14 -0800

    Started work on VIP Defense mission

---
+++ Assets/Scripts/General/VIPMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class VIPMovement : MonoBehaviour
+{
+	[SerializeField] float engineForce = 25f;
+	[SerializeField] float targetReachedDistance = 10f;
+	MissionManager missionManager;
+	Transform travelEndPoint;
+	Vector3 targetPosition;
+	float currentForce = 0f;
+	float forceMultiplier = 1f;
+	float timeModifier = 120f;
+	float torqueModifier = 0.12f;
+	float randomEngineForce;
+	float angle;
+	Vector3 offset;
+
+	void Start()
+	{
+		float forceRandomizer = Random.Range(0.9f, 1.1f);
+		randomEngineForce = engineForce * forceRandomizer;
+		targetPosition = travelEndPoint.position;
+
+		var missionManagerObject = GameObject.FindGameObjectWithTag("MissionManager");
+		
+		if (missionManagerObject != null)
+			missionManager = missionManagerObject.GetComponent<MissionManager>();
+	}
+
+	void FixedUpdate()
+	{
+		currentForce = Mathf.MoveTowards(currentForce, randomEngineForce * forceMultiplier, timeModifier * Time.fixedDeltaTime);
+		rigidbody.AddForce(transform.forward * currentForce, ForceMode.Acceleration);
+		var offset = transform.InverseTransformPoint(targetPosition);
+		angle = Mathf.Rad2Deg * Mathf.Atan2(offset.x, offset.z);
+
+		Vector2 targetXZPosition = new Vector2(travelEndPoint.position.x, travelEndPoint.position.z);
+		Vector2 unitXZPosition = new Vector2(transform.position.x, transform.position.z);
+		float distance = Vector2.Distance(targetXZPosition, unitXZPosition);
+
+		if (Mathf.Abs(angle) > 3f && distance > targetReachedDistance)
+		{
+			rigidbody.AddTorque(Vector3.up * torqueModifier * Mathf.Sign(angle), ForceMode.VelocityChange);
+		}
+	}
+}
