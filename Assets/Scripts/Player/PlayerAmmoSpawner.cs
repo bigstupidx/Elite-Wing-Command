01358a01d0f0b38c6bf7e3abf45a0ccaeb7e31c8
Kevin Salchert <kevin.salchert@me.com>
2014-02-15 14:32:28 -0800

    Multiple types of player bullets

--- Assets/Scripts/Player/PlayerAmmoSpawner.cs
+++ Assets/Scripts/Player/PlayerAmmoSpawner.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class PlayerAmmoSpawner : AmmoSpawner
 {
 	[SerializeField] int weaponSlotNumber = 1;
-	[SerializeField] Rigidbody redBullet;
-	[SerializeField] Rigidbody blueBullet;
+	[SerializeField] Rigidbody allyBullet1;
+	[SerializeField] Rigidbody allyBullet2;
 	int weaponCaseSwitch;
 
 	void Awake()
@@ -15,12 +15,12 @@ public class PlayerAmmoSpawner : AmmoSpawner
 		switch(PlayerPrefs.GetInt("Weapon Equip " + weaponSlotNumber.ToString()))
 		{
 		case 1:
-			Weapon = redBullet;
+			Weapon = allyBullet1;
 			FireRate = 0.12f;
 			Force = 60f;
 			break;
 		case 2:
-			Weapon = blueBullet;
+			Weapon = allyBullet2;
 			FireRate = 0.2f;
 			Force = 50f;
 			break;
