0471367747e2752f881d8579d2fb33e605f98a89
Kevin Salchert <kevin.salchert@me.com>
2014-03-06 22:29:28 -0800

    Created Mission Complete menu

---
+++ Assets/Scripts/Menu/ScoreTextNumbers.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreTextNumbers : MonoBehaviour
+{
+	[SerializeField] UILabel missionScoreObject;
+	
+	void Start()
+	{
+		string missionScores = "____________________________\n\n25\n\n8\n\nx4\n\nx5";
+		missionScoreObject.text = missionScores;
+	}
+}
