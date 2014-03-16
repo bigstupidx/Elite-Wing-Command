881144347668db824ed04520e4a410748c08132f
Kevin Salchert <kevin.salchert@me.com>
2014-03-16 12:02:05 -0700

    Created options menu and script to transition between menu screens

---
+++ Assets/Scripts/Menu/MenuTransition.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuTransition : MonoBehaviour
+{
+	[SerializeField] GameObject currentScreen;
+	[SerializeField] GameObject nextScreen;
+	[SerializeField] float tweenTime = 0.5f;
+	[SerializeField] GameObject altCamera;
+	[SerializeField] bool enableAltCamera = false;
+
+	void OnClick()
+	{
+		StartCoroutine(TransitionToNext());
+	}
+
+	IEnumerator TransitionToNext()
+	{
+		TweenAlpha currentTweenAlpha = currentScreen.GetComponent<TweenAlpha>();
+		currentTweenAlpha.from = 1f;
+		currentTweenAlpha.to = 0f;
+		currentTweenAlpha.ResetToBeginning();
+		currentTweenAlpha.enabled = true;
+		yield return new WaitForSeconds (tweenTime);
+
+		if (altCamera != null)
+			altCamera.SetActive(enableAltCamera);
+
+		TweenAlpha nextTweenAlpha = nextScreen.GetComponent<TweenAlpha>();
+		nextTweenAlpha.from = 0f;
+		nextTweenAlpha.to = 1f;
+		nextTweenAlpha.ResetToBeginning();
+		nextTweenAlpha.enabled = true;
+		nextScreen.SetActive(true);
+		yield return new WaitForSeconds(tweenTime);
+
+		currentTweenAlpha.enabled = false;
+		nextTweenAlpha.enabled = false;
+		currentScreen.SetActive(false);
+	}
+}
