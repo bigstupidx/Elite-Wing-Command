d7991ffd85a9d8c73bf94d212e4c4864bfa5806b
Kevin Salchert <kevin.salchert@me.com>
2014-03-19 01:03:09 -0700

    Additional Hanger menu integration

---
+++ Assets/Scripts/Menu/Hanger/HangerButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HangerButton : MonoBehaviour
+{
+	[SerializeField] bool defaultButton;
+	[SerializeField] GameObject buttonActivateContents;
+	[SerializeField] GameObject[] buttonDeactivateContents;
+	Vector3 tempActivatedScale;
+	Vector3 tempDeactivatedScale;
+
+	void OnEnable()
+	{
+		if (defaultButton)
+			OnClick();
+	}
+
+	void OnClick()
+	{
+		buttonActivateContents.SetActive(true);
+
+		foreach(GameObject contentsObject in buttonDeactivateContents)
+			contentsObject.SetActive(false);
+	}
+}
