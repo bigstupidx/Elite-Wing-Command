faafbf7c9310545185526fd16e97e7f96e46bec7
Kevin Salchert <kevin.salchert@me.com>
2014-04-17 08:17:37 -0700

    More audio bug fixes

---
+++ Assets/Scripts/Audio/DelayStartAudioAtStart.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayStartAudioAtStart : MonoBehaviour
+{
+	[SerializeField] string eventName;
+	[SerializeField] float waitTime = 0.3f;
+
+	void Start()
+	{
+		StartCoroutine(WaitAndPlay());
+	}
+
+	IEnumerator WaitAndPlay()
+	{
+		yield return new WaitForSeconds(waitTime);
+		Fabric.EventManager.Instance.PostEvent(eventName, Fabric.EventAction.PlaySound);
+	}
+}
--- Assets/Scripts/Menu/LoadLevel.cs
+++ Assets/Scripts/Menu/LoadLevel.cs
@@ -9,6 +9,7 @@ public class LoadLevel : MonoBehaviour
 	void OnClick()
 	{
 		StartCoroutine(WaitAndLoad());
+		Fabric.EventManager.Instance.PostEvent("SFX", Fabric.EventAction.StopAll);
 		Fabric.EventManager.Instance.PostEvent("SFX_Button_General", Fabric.EventAction.PlaySound);
 	}
 
