883e22bb6711f345398fb7c226218af2a9962017
Kevin Salchert <kevin.salchert@me.com>
2014-04-21 10:41:00 -0700

    Additional audio bug fixes

---
+++ Assets/Scripts/Audio/MenuAudioFadeIn.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAudioFadeIn : MonoBehaviour
+{
+	[SerializeField] string eventName = "Music_Menu";
+	[SerializeField] float fadeInTime;
+	float currentVolume;
+	bool fadeIn = false;
+
+	void Start()
+	{
+		currentVolume = 0f;
+		Fabric.EventManager.Instance.PostEvent(eventName, Fabric.EventAction.SetVolume, currentVolume);
+		StartCoroutine(WaitAndPlay());
+	}
+
+	IEnumerator WaitAndPlay()
+	{
+		yield return new WaitForSeconds(0.25f);
+		Fabric.EventManager.Instance.PostEvent(eventName, Fabric.EventAction.PlaySound);
+		fadeIn = true;
+	}
+
+	void Update()
+	{
+		if (fadeIn)
+		{
+			currentVolume += Time.deltaTime/fadeInTime;
+
+			if (currentVolume >= 0.63f)
+			{
+				currentVolume = 0.63f;
+				fadeIn = false;
+			}
+
+			Fabric.EventManager.Instance.PostEvent(eventName, Fabric.EventAction.SetVolume, currentVolume);
+		}
+	}
+}
