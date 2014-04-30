380e21cfabb47fb0edae460622e8d6a8ee082de2
Kevin Salchert <kevin.salchert@me.com>
2014-04-30 11:34:04 -0700

    Implemented final mission completion reward

---
+++ Assets/Scripts/Menu/FinalMissionReward.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalMissionReward : MonoBehaviour
+{
+	[SerializeField] GameObject rewardPanel;
+	[SerializeField] int rewardBonusTotal;
+
+	void Awake()
+	{
+		if (EncryptedPlayerPrefs.GetInt("Mission 310 Status", 0) == 1 && EncryptedPlayerPrefs.GetInt("Final Mission Bonus Status", 0) == 0)
+		{
+			float rewardPoints = EncryptedPlayerPrefs.GetFloat("Reward Points", 0);
+			rewardPoints += rewardBonusTotal;
+			EncryptedPlayerPrefs.SetFloat("Reward Points", rewardPoints);
+			rewardPanel.SetActive(true);
+			EncryptedPlayerPrefs.SetInt("Final Mission Bonus Status", 1);
+		}
+	}
+}
