91ecfa6b70ad12fb877227195e3ab8a61cb1d726
Kevin Salchert <kevin.salchert@me.com>
2014-05-07 22:44:09 -0700

    Removed Fabric tutorials, misc. bug fixes and optimizations

---
+++ Assets/Scripts/General/FreezeTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreezeTime : MonoBehaviour
+{
+
+	void Start()
+	{
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.F))
+			CustomTimeManager.FadeTo(0f, 0.01f);
+		else if (Input.GetKeyDown(KeyCode.R))
+			CustomTimeManager.FadeTo(1.1f, 0.01f);
+	}
+}
