a5279dc12600a99b3f4c607567721e4291bcbb91
Kevin Salchert <kevin.salchert@me.com>
2014-05-19 13:04:14 -0700

    Base vs. Base missions now spawn tanks, not multipurpose

--- Assets/Scripts/Missions/MissionSetComplete.cs
+++ Assets/Scripts/Missions/MissionSetComplete.cs
@@ -9,15 +9,26 @@ public class MissionSetComplete : MonoBehaviour
 		int missionNumber = EncryptedPlayerPrefs.GetInt("Mission Number", 0);
 		EncryptedPlayerPrefs.SetInt("Mission " + missionNumber.ToString() + " Status", 1);
 
+		var gameCenterObject = GameObject.FindGameObjectWithTag("GameCenter");
+
 		if (missionNumber == 101)
 		{
-			var gameCenterObject = GameObject.FindGameObjectWithTag("GameCenter");
 			
 			if (gameCenterObject != null)
 			{
 				PlayerPrefs.Save();
 				EWCGameCenter gameCenterScript = gameCenterObject.GetComponent<EWCGameCenter>();
 				gameCenterScript.SubmitAchievement("complete_first_mission", 100f);
+			}
+		}
+		else if (missionNumber == 310)
+		{
+			
+			if (gameCenterObject != null)
+			{
+				PlayerPrefs.Save();
+				EWCGameCenter gameCenterScript = gameCenterObject.GetComponent<EWCGameCenter>();
+				gameCenterScript.SubmitAchievement("complete_all_campaign_missions", 100f);
 			}
 		}
 	}
